fe581c2a5310714b76bf0bc37481beddbd1c61b4
Jesse Brink <jbrink0925@gmail.com>
2021-10-15 20:29:46 -0400

    create add and remove Todos methods

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,2 +1,20 @@
 import React, { useState } from "react";
 import Todo from "./Todo";
+import "./TodoList.css";
+
+const TodoList = () => {
+    const [todos, updateTodos] = useState([]);
+    const addTodo = newTodo => {
+        updateTodos([
+            ...todos,
+            newTodo
+        ]);
+    };
+
+    const removeTodo = oldTodo => {
+        updateTodos(() => {
+            const filteredTodos = todos.filter(todo => todo !== oldTodo);
+            return filteredTodos;
+        });
+    };
+};
